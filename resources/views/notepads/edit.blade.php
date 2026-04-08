17640d3cec6b6737e4dee1e93aa78483aee7beb0
JKJapona <j.japona.558573@umindanao.edu.ph>
2026-04-08 15:25:42 +0800

    feat: updated css #2

--- resources/views/notepads/edit.blade.php
+++ resources/views/notepads/edit.blade.php
@@ -5,19 +5,19 @@
 <div class="note-container">
     <h1>Edit Notepad</h1>
     
-    <form action="{{ route('notepads.update', $notepad->id) }}" method="POST">
+    <form action="{{ route('notepads.update', $notepad->id) }}" method="POST" class="note-form">
         @csrf
         @method('PUT')
         <div>
-            <label>Title:</label><br>
+            <label>Title:</label>
             <input type="text" name="title" value="{{ $notepad->title }}">
         </div>
 
         <div>
-            <label>Body:</label><br>
+            <label>Body:</label>
             <textarea name="body">{{ $notepad->body }}</textarea>
         </div>
-        
+
         <button type="submit">Update</button>
         <a href="{{ route('notepads.index') }}">
             <button type="button">Back</button>
